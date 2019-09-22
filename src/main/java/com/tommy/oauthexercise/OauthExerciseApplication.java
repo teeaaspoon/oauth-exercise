1c4e453cb1b0d7f8f0e2184c12d02a51d9412b37
Tommy Poon <tommypoon@Tommys-MacBook-Pro.local>
2019-09-22 10:48:21 -0400

    first commit

---
+++ src/main/java/com/tommy/oauthexercise/OauthExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.tommy.oauthexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthExerciseApplication.class, args);
+	}
+
+}
